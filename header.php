fa37e3fc12a196ac9d8684389bf57940586ae149
Hari Krishnan <hari.krishnan17@outlook.com>
2017-08-01 16:44:42 -0700

    Test header 1

---
+++ header.php
@@ -0,0 +1,73 @@
+<header id="header">      
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 overflow">
+                   <div class="social-icons pull-right">
+                        <ul class="nav nav-pills">
+                            <li><a href="https://www.facebook.com/profile.php?id=622149401"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/harikrishnan17"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div> 
+                </div>
+             </div>
+        </div>
+        
+        <div class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <a class="navbar-brand" href="index.html">
+                        <h1><img src="images/hari.png" alt="logo"></h1>
+                    </a>
+                    
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        
+                        <li><a href="aboutme.html">About  Me</a></li>
+                 
+                        <li class="dropdown"><a href="portfolio.html">Projects <i class="fa fa-angle-down"></i></a>
+                            <ul role="menu" class="sub-menu">
+                                <li><a href="portfolio.html">All Projects</a></li>
+                            </ul>
+                        </li> 
+                        
+                        <li><a href="theatre.html">Theatre </a></li>
+                        </li>
+
+                        <li><a href="social.html">Social </a></li>
+                        </li>
+ 
+                        <li><a href="creative.html">Creative </a></li>
+                        </li>
+
+                        <li><a href="Resume.html">Resume </a></li>
+                        </li>
+
+                        <li><a href="Adventure.html">Adventure </a></li>
+                        </li>
+
+                        <li><a href="https://igotthinking.wordpress.com/">Blog </a></li>
+                        </li>
+              
+
+                    </ul>
+                </div>
+                <div class="search">
+                    <form role="form">
+                        <i class="fa fa-search"></i>
+                        <div class="field-toggle">
+                            <input type="text" class="search-form" autocomplete="off" placeholder="Search">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</header>
